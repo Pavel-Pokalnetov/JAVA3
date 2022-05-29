f1ef2ace0d74b18ec68fecd7961354eb099be6df
Покальнетов Павел Сергеевич <redx@mail.ru>
2022-05-29 22:04:52 +0500

    задание 1

---
+++ src/main/java/ru/gb/java3/homework5/MainRace.java
@@ -0,0 +1,23 @@
+import ru.gb.java3.homework5.Car;
+import ru.gb.java3.homework5.Race;
+import ru.gb.java3.homework5.Road;
+import ru.gb.java3.homework5.Tunnel;
+
+public class MainRace {
+    public static final int CARS_COUNT = 4;
+
+    public static void main(String[] args) {
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
+        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        Car[] cars = new Car[CARS_COUNT];
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+        }
+        for (int i = 0; i < cars.length; i++) {
+            new Thread(cars[i]).start();
+        }
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+    }
+}
+
