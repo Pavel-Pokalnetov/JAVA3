8be417488050b416a4d84476e04c3c2b07dd6433
Покальнетов Павел Сергеевич <redx@mail.ru>
2022-06-01 18:02:04 +0500

    меттод обработки массива

--- src/main/java/ru/gb/java3/homework6/Main.java
+++ src/main/java/ru/gb/java3/homework6/Main.java
@@ -1,5 +1,25 @@
 package ru.gb.java3.homework6;
 
-public class Main {
+import java.util.Arrays;
 
+public class Main {
+    public static Integer[] remakeArray(Integer[] array){
+        boolean flag = false;
+        int i;
+        for (i = array.length-1; i =0; i--) {
+             if (array[i]==4){
+                 flag=true;
+                 break;
+             }
+        }
+        if (!flag){
+            throw new RuntimeException();
+        }
+        return Arrays.copyOfRange(array,i,array.length-1);
+
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
