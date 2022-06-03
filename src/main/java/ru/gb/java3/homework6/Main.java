37f7d8826ba19dec3ac4f15c2f51f206ac17d073
Покальнетов Павел Сергеевич <redx@mail.ru>
2022-06-03 12:25:56 +0500

    меттод обработки массива(2) + проверки массива (3)

--- src/main/java/ru/gb/java3/homework6/Main.java
+++ src/main/java/ru/gb/java3/homework6/Main.java
@@ -3,23 +3,37 @@ package ru.gb.java3.homework6;
 import java.util.Arrays;
 
 public class Main {
-    public static Integer[] remakeArray(Integer[] array){
+    private Integer[] remakeArray(Integer[] array) {
         boolean flag = false;
         int i;
-        for (i = array.length-1; i =0; i--) {
-             if (array[i]==4){
-                 flag=true;
-                 break;
-             }
+        for (i = array.length - 1; i >= 0; i--) {
+            if (array[i] == 4) {
+                flag = true;
+                break;
+            }
         }
-        if (!flag){
+        if (i == array.length - 1 || !flag) {
             throw new RuntimeException();
         }
-        return Arrays.copyOfRange(array,i,array.length-1);
+        return Arrays.copyOfRange(array, i + 1, array.length - 1);
 
     }
 
+    private boolean checkArray(Integer[] array) {
+        boolean n1 = false, n4 = false;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == 1) {
+                n1 = n1 | true;
+            } else if (array[i] == 4) {
+                n4 = n4 | true;
+            }
+            if (n1 & n4) return true;
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
+
 
     }
 }
